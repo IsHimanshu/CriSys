fc358a38665b7126d5b7f7e53490a1734f26568b
IsHimanshu <51610605+IsHimanshu@users.noreply.github.com>
2023-12-06 14:21:52 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,7 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
-import { firebaseConfig } from "./config";
+
+
+const firebaseConfig = {
+  apiKey: process.env.api_key,
+  authDomain: process.env.auth_domain,
+  projectId: process.env.project_id,
+  storageBucket: process.env.storage_bucket,
+  messagingSenderId: process.env.messaging_sender_id,
+  appId: process.env.app_id,
+  measurementId: process.env.measurement_id
+};
 
 
 const app = initializeApp(firebaseConfig);
