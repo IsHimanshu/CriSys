9736b91f5b35382fbaeb991067ed640bb23344eb
IsHimanshu <51610605+IsHimanshu@users.noreply.github.com>
2024-05-15 02:42:24 +0530

    Update alert.py

--- src/alert/alert.py
+++ src/alert/alert.py
@@ -17,14 +17,3 @@ def send_sms_alert(account_sid, auth_token, from_number, to_number, message):
 
     except Exception as e:
         print(f"Error sending SMS alert: {str(e)}")
-
-
-# Example usage
-account_sid = "ACcba4c2b4401afdb6efcc364e36f4148b"
-auth_token = "7a0a5d56280c619b6321da716a37338f"
-from_number = "+12543234503"  # Twilio Sandbox number provided during the trial
-numbers = ["+918423068386", "+918920419037", "+917903863776", "+916388374850"]
-to_number = numbers  # Recipient's phone number
-message = "This is an SMS alert."
-for i in numbers:
-    send_sms_alert(account_sid, auth_token, from_number, i, message)
